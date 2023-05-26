6dbe9cc5e1b007f0b6f11cfcc7a5aa06837e188b
Da Shen <da@tubi.tv>
2023-05-26 15:01:06 +0800

    hello/primitive/primitive_1

--- hello.rs
+++ hello.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello World!")
+    let msg = "Hello World!";
+    let zh_msg = "你好，世界！";
+    println!("{}", msg);
+    for c in zh_msg.chars() {
+        println!("{c}")
+    }
 }
--- xmake.lua
+++ xmake.lua
@@ -4,3 +4,10 @@ target("hello")
     set_kind("binary")
     add_files("hello.rs")
 
+target("primitive")
+    set_kind("binary")
+    add_files("primitive.rs")
+
+target("primitive_1")
+    set_kind("binary")
+    add_files("primitive_1.rs")
